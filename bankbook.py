93a002e850cf94bfb62797d234b0fbe5c07cb7b5
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2023-01-30 17:50:43 +0900

    ローカルでの入力をfirestoreに保存できるようにした

--- bankbook.py
+++ bankbook.py
@@ -1,28 +1,13 @@
-import pandas as pd
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 import get_data
 import put_data
-import datetime
-
-# サイン項目が選択された時
-
-# 　保存ボタン
+import firebase_admin
+from firebase_admin import credentials
 
 
-def save():
-    frame.destroy()
-    trading_entry_frame = ttk.Frame(root)
-    trading_entry_frame.grid(row=0, column=0)
-    items = ['日付', '預ける', '引き出す', '利子', '合計', 'サイン']
-    for i in range(0, len(items)):
-        label_item = ttk.Label(trading_entry_frame,
-                               text=items[i], anchor=tk.CENTER)
-        label_item.grid(row=0, column=i)
-
-
-def sign_entry(event):
+def sign_entry_check(event):
     print('サイン入力欄が選択された')
 
     # パスワード入力画面を表示させる
@@ -55,8 +40,6 @@ def trading_history():
     trading_history_frame = ttk.Frame(root)
     trading_history_frame.grid(row=0, column=0)
 
-
-
     get_data_list = get_data.get_data()
     trade_list = []
     for doc in get_data_list:
@@ -72,13 +55,8 @@ def trading_history():
     n = len(trade_list)
 
     for i in range(0, n):
-        date = datetime.datetime.fromtimestamp(
-            trade_list[i].get('date').timestamp()).date()
-        date = str(date).split('-')
-        date = date[0]+'年'+date[1]+'月'+date[2]+'日'
-        print(date)
-        date_text = ttk.Label(trading_history_frame,
-                              text=date, width=12, padding=[30, 5, 0, 5])
+        date = trade_list[i].get('date')
+        date_text = ttk.Label(trading_history_frame, text=date, width=12, anchor=tk.CENTER)
         date_text.grid(row=i+1, column=0)
 
     # 預ける項目
@@ -115,91 +93,52 @@ def trading_history():
         sign = trade_list[i].get('sign')
         sign_text = ttk.Label(trading_history_frame,
                               text=sign, width=12, anchor=tk.CENTER)
-        # TODO sign.bind('<Button-1>', sign_entry)
+        # TODO sign.bind('<Button-1>', sign_entry_check)
         sign_text.grid(row=i+1, column=5)
 
-    # 実行ボタン
-
-    # def execute():
-    #     # これまでの入力をリストに入れる
-    #     table_data = []
-    #     for i in range(0, n):
-    #         table_data.append([dates[i].get(),
-    #                            deposits[i].get(),
-    #                            withdraws[i].get(),
-    #                            interests[i].get(),
-    #                            totals[i].get(),
-    #                            signs[i].get()
-    #                            ])
-    #     # pandas dataframeに変換
-    #     df = pd.DataFrame(table_data, columns=[
-    #                       '日付', '預ける', '引き出す', '利子', '合計', 'サイン',])
-
-    #     # 結果表示
-    #     output(df)
-
     button = ttk.Button(root,
-                        text='保存',
+                        text='入力',
                         padding=5,
-                        command=save)
+                        command=put_data_entry)
     button.grid(row=1, column=0)
     root.mainloop()
 
 
-def output(df):
+def put_data_entry():
     root = tk.Tk()
     root.title('保存内容')
+    root.geometry('625x150')
+    items = ['日付', '預ける', '引き出す', '利子', '合計', 'サイン']
+    for i in range(0, len(items)):
+        label_item = ttk.Label(root,
+                               text=items[i])
+        label_item.grid(row=0, column=i)
 
-    trading_history_frame = ttk.Frame(root, padding=5)
-    trading_history_frame.grid(row=0, column=0)
-
-    # ツリービューの作成
-    tree = ttk.Treeview(trading_history_frame)
-
-    # 列インデックスの作成
-    tree['columns'] = (0, 1, 2, 3, 4, 5)
-    # 表スタイルの設定(headingsは通常の表形式)
-    tree['show'] = 'headings'
-    # 各列の設定(インデックス,オプション(今回は幅を指定))
-    for i in range(0, 6):
-        tree.column(i, width=100)
-
-    # 各列のヘッダー設定(インデックス,テキスト)
-    tree.heading(0, text='日付')
-    tree.heading(1, text='預ける')
-    tree.heading(2, text='引き出す')
-    tree.heading(3, text='利子')
-    tree.heading(4, text='合計')
-    tree.heading(5, text='サイン')
-
-    for i in range(0, len(df)):
-        date = df.iloc[i][0]
-        deposit = df.iloc[i][1]
-        withdraw = df.iloc[i][2]
-        interest = df.iloc[i][3]
-        total = df.iloc[i][4]
-        sign = df.iloc[i][5]
-
-        # レコードの作成
-        # 1番目の引数-配置場所（表形式の表示ではブランクとする）
-        # 2番目の引数-end:表の配置順序を最下部に配置
-        # (行インデックス番号を指定することもできる)
-        # 3番目の引数-values:レコードの値をタプルで指定する
-        tree.insert('', 'end', values=(
-            date, deposit, withdraw, interest, total, sign))
-
-    # ツリービューの配置
-    tree.grid(row=0)
+    datetime_entry = ttk.Entry(root, width=10)
+    datetime_entry.grid(row=1, column=0)
+    deposit_entry = ttk.Entry(root, width=10)
+    deposit_entry.grid(row=1, column=1)
+    withdraw_entry = ttk.Entry(root, width=10)
+    withdraw_entry.grid(row=1, column=2)
+    interest_entry = ttk.Entry(root, width=10)
+    interest_entry.grid(row=1, column=3)
+    total_entry = ttk.Entry(root, width=10)
+    total_entry.grid(row=1, column=4)
+    sign_entry = ttk.Entry(root, width=10)
+    sign_entry.bind('<ButtonPress>', sign_entry_check)
+    sign_entry.grid(row=1, column=5)
 
     button = ttk.Button(root,
                         text='入力',
-                        command=save)
-    button.grid(row=2, pady=5)
+                        command= lambda : put_data.put_data(datetime_entry.get(), deposit_entry.get(), withdraw_entry.get(), interest_entry.get(), total_entry.get(), sign_entry.get()))
+    button.place(x=265, y=100)
 
     root.mainloop()
 
-
 if __name__ == '__main__':
+    cred = credentials.Certificate("./dan-bankbook-firebase-adminsdk-hjd6u-8c0fe69935.json")
+    firebase_admin.initialize_app(cred)
+    
     root = tk.Tk()
     root.title('ダン通帳')
     root.geometry('700x482')
@@ -210,7 +149,7 @@ if __name__ == '__main__':
     back_image = tk.PhotoImage(file='back_image.png', width=700, height=450)
     canvas.create_image(0, 0, image=back_image, anchor=tk.NW)
     next_button = tk.Button(frame, text='取引ページへ',
-                            bg='#ffffff', command=trading_history)
+                            bg='#ffffff', command=trading_history())
     next_button.pack()
 
     root.mainloop()
